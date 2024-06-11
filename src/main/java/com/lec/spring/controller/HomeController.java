965a95ce3e85c35e05e068b0625ca2df2dbf8b6b
eunjeong0911 <eunjeong0911@naver.com>
2024-06-11 10:40:58 +0900

    Initial Commit

--- src/main/java/com/lec/spring/controller/HomeController.java
+++ src/main/java/com/lec/spring/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @Slf4j
 @RestController
 public class HomeController {
